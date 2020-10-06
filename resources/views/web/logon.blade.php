92407d98330fd8441080d2e948ef5b4693158f96
João Pedro <joaopedro453@hotmail.com>
2020-10-06 20:23:16 -0300

    add web logon

---
+++ resources/views/web/logon.blade.php
@@ -0,0 +1,20 @@
+@extends('web._template', ['title' => 'Faça Logon'])
+
+
+@section('content')
+    <div class="logon-container">
+        <section class="form">
+            <img src="{{url('web/images/logo.svg')}}" alt="Be The Hero" title="Be The Hero"/>
+            <form onSubmit={this.handleLogin}>
+                <h1>Faça seu Logon</h1>
+                <input
+                    placeholder="Sua ID"
+                    type="text"/>
+                <button class="button">Entrar</button>
+
+                <a class="back-link" href="/register">Não tenho cadastro </a>
+            </form>
+        </section>
+        <img src="{{url('web/images/heroes.png')}}" alt="heroes"/>
+    </div>
+@endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+mix.js('resources/views/web/js/app.js', 'public/web/js/scripts.js')
+    .postCss('resources/views/web/css/app.css', 'public/web/css/styles.css', [
         //
-    ]);
+    ]).version();

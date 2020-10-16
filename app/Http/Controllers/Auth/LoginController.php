d00e5f0643954af056d0cccad796cae05487d2fe
João Pedro <joaopedro453@hotmail.com>
2020-10-16 11:56:15 -0300

    add Auth/LoginController

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed...
+            return redirect()->intended('profile');
+        }
+        return redirect(route('web.logon'));
+    }
+
+    /**
+     * Logout user
+     */
+    public function logout(): void
+    {
+        Auth::logout();
+    }
+}

d46693d88ff1a8eeb122f7fb1c3208d917d87378
João Pedro <joaopedro453@hotmail.com>
2020-10-18 12:22:07 -0300

    add home page

---
+++ resources/views/web/home.blade.php
@@ -0,0 +1,30 @@
+@extends('web._template')
+
+@section('content')
+    <div class="logon-container">
+        <section class="form">
+            <img src="{{url('web/images/logo.svg')}}" alt="Be The Hero" title="Be The Hero"/>
+            <div style="padding-top: 25px">
+                <button id="help" class="button">Quero Ajudar!</button>
+                <button id="ong" class="button">Sou uma ONG</button>
+            </div>
+        </section>
+        <img src="{{url('web/images/heroes.png')}}" alt="heroes"/>
+    </div>
+
+
+    <script>
+        const ong = document.getElementById('ong');
+        ong.addEventListener('click', function () {
+            window.location.href = '{{route('web.logon')}}'
+        });
+
+        const help = document.getElementById('ong');
+        help.addEventListener('click', function () {
+            window.location.href = '{{route('web.logon')}}'
+        })
+    </script>
+@endsection
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function () {
+    return view('web.home');
+})->name('web.home');
+
 Route::get('/', [App\Http\Controllers\WebController::class, 'Logon'])->middleware('guest')
     ->name('web.logon');
 Route::post('/', [\App\Http\Controllers\Auth\LoginController::class, 'authenticate'])->name('web.post.logon');

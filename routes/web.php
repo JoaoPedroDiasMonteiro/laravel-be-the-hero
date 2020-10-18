2849fcb4670b7efa010969071395bceaea414f40
João Pedro <joaopedro453@hotmail.com>
2020-10-18 12:26:15 -0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', function () {
+Route::get('/', function () {
     return view('web.home');
 })->name('web.home');
 
-Route::get('/', [App\Http\Controllers\WebController::class, 'Logon'])->middleware('guest')
+
+/*
+ * ONG Routes
+ */
+
+// Auth Routes
+Route::get('/login', [App\Http\Controllers\WebController::class, 'Logon'])->middleware('guest')
     ->name('web.logon');
-Route::post('/', [\App\Http\Controllers\Auth\LoginController::class, 'authenticate'])->name('web.post.logon');
+Route::post('/login', [\App\Http\Controllers\Auth\LoginController::class, 'authenticate'])->name('web.post.logon');
 
 Route::get('/logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('web.logout');
 
@@ -28,7 +34,6 @@ Route::get('/register', [App\Http\Controllers\WebController::class, 'register'])
 Route::post('/register', [\App\Http\Controllers\Auth\UserController::class, 'register'])->name('web.post.register');
 
 // User Routes
-
 Route::get('/profile', [App\Http\Controllers\WebController::class, 'profile'])->name('web.profile')
     ->middleware('auth');
 
@@ -36,6 +41,7 @@ Route::get('/incident/new', [App\Http\Controllers\WebController::class, 'newInci
     ->name('web.incident.new');
 Route::post('/incident', [App\Http\Controllers\WebController::class, 'storeIncident'])->middleware('auth')
     ->name('web.incident.store');
+
 Route::delete('/incident/{incident}', [App\Http\Controllers\WebController::class, 'destroyIncident'])->middleware('auth')
     ->name('web.incident.destroy');
 
